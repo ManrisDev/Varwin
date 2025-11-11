f026a368382c2b747d22690b69c7e463a1ef0d81
Manris <polesko02@mail.ru>
2025-11-11 23:32:00 +0300

    Build
    1. Перезапек сцену
    2. Сделал практически все объекты интерактивными
    3. Собрал лампу и громофон
    4. Убрал лишние скайбоксы

---
+++ Assets/Objects/LightDesk/LightDesk.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Varwin;
+using Varwin.Public;
+
+namespace Varwin.Types.LightDesk_a4ee68c0cf654ca4b181536644315523
+{
+    [VarwinComponent(English: "Light Desk", Russian: "")]
+    public class LightDesk : VarwinObject
+    {
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+             
+        }
+    }
+}
