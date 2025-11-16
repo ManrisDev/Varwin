2298468483b5e68eb9bdc97c0171203a2117d29b
Manris <polesko02@mail.ru>
2025-11-16 21:34:20 +0300

    Подготовка к изменениям

---
+++ Assets/Objects/Chair/Chair.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Varwin;
+using Varwin.Public;
+
+namespace Varwin.Types.Chair_863d29615b8f47538b8bd5f46fb7aa9f
+{
+    [VarwinComponent(English: "Chair")]
+    public class Chair : VarwinObject
+    {
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+             
+        }
+    }
+}
