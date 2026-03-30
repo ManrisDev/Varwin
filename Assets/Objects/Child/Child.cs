ece699ffd55b79c325b0c39e21a0fa40cd566e35
Manris <polesko02@mail.ru>
2026-03-30 16:23:19 +0300

    Load

---
+++ Assets/Objects/Child/Child.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Varwin;
+using Varwin.Public;
+
+namespace Varwin.Types.Child_93994bf175ae4ff19efbf5c24a1fb2e1
+{
+    [VarwinComponent(English: "Playground")]
+    public class Child : VarwinObject
+    {
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+             
+        }
+    }
+}
